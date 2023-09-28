719f12c3a3afde698bae92710ab4596fb833a0bb
Naimhz <sekki691@gmail.com>
2023-09-28 11:36:22 +0200

    Base du projet + navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom'
+import '../../style/Navbar.scss'
+import HomeLogo from '../../assets/HomeLogo.svg'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={HomeLogo} alt="Logo du site" className="navbar__logo" />
+      <div className="navbar__links">
+        <NavLink
+          to="/"
+          activeclassname="active"
+          className="navbar__links--link"
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/About"
+          activeclassname="active"
+          className="navbar__links--link"
+        >
+          A propos
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
